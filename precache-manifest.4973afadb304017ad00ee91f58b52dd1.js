4dcee41a48989919ca52f29b2e9b09703b021f05
abdoabdoabdoabdoabdo <abdonaggar77@gmail.com>
2020-08-10 13:31:31 +0200

    Updates

---
+++ precache-manifest.4973afadb304017ad00ee91f58b52dd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fd3f8f58bd67eaa7c355a84d20df9e",
+    "url": "/github.io/coolapp/index.html"
+  },
+  {
+    "revision": "02a8b0b24f8fd8d4baa9",
+    "url": "/github.io/coolapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/github.io/coolapp/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/coolapp/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a8b0b24f8fd8d4baa9",
+    "url": "/github.io/coolapp/static/js/main.526cdded.chunk.js"
+  },
+  {
+    "revision": "868f42c9d15f48719383",
+    "url": "/github.io/coolapp/static/js/runtime-main.cf6fce14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/coolapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
